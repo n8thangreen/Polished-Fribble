b8e15b6d0032f9788b53ef6c099b71bc90de2a13
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-06-10 10:14:23 +0100

    simple reformatting

--- SQL stuff_v0.2.R
+++ SQL stuff_v0.2.R
@@ -3,7 +3,13 @@ library(RSQLite)
 library(DBI)
 
 
-update <- function(id,date,day_of_the_week,available_period,notes,room_table='room_table') {
+update <- function(id,
+                   date,
+                   day_of_the_week,
+                   available_period,
+                   notes,
+                   room_table = 'room_table') {
+  
   db <- dbConnect(SQLite(), 'room_store.sqlite')
   query <- paste0("UPDATE ",room_table," SET ",
                   "Available_period = '", available_period , "', ",
@@ -16,26 +22,31 @@ update <- function(id,date,day_of_the_week,available_period,notes,room_table='ro
 }
 
 
-
-
-
-create <- function(id,date,day_of_the_week,available_period,notes,room_table='room_table') {
+create <- function(id,
+                   date,
+                   day_of_the_week,
+                   available_period,
+                   notes,
+                   room_table = 'room_table') {
+  
   db <- dbConnect(SQLite(), 'room_store.sqlite')
-  table=dbReadTable(db,'room_table')
-  com <- paste0("(", id , ", ",
-                "'", date, "','",
-                day_of_the_week,
-                "','", available_period,"','",notes,"')")
-  query=paste0("INSERT INTO ",room_table," VALUES ",paste(com, collapse = ", "))
+  table <- dbReadTable(db, 'room_table')
+  com <- paste0("(",
+                id , ", ", "'",
+                date, "','",
+                day_of_the_week, "','",
+                available_period,"','",
+                notes,"')")
+  query <- paste0("INSERT INTO ",room_table,
+                  " VALUES ",paste(com, collapse = ", "))
   print(query)#for debug
   dbGetQuery(db, query)
   dbDisconnect(db)
 }
 
 
-
 #Load data currently saved in sqlite
-loadData <- function(room_table='room_table') {
+loadData <- function(room_table = 'room_table') {
   db <- dbConnect(SQLite(), "room_store.sqlite")
   query <- sprintf("SELECT * FROM %s", room_table)
   data <- dbGetQuery(db, query)
@@ -44,9 +55,14 @@ loadData <- function(room_table='room_table') {
 }
 
 #Deletion
-delete <- function(id,date,day_of_the_week,room_table='room_table') {
+delete <- function(id,
+                   date,
+                   day_of_the_week,
+                   room_table = 'room_table') {
+  
   db <- dbConnect(SQLite(), "room_store.sqlite")
-  query <- paste0('DELETE FROM ',room_table,' WHERE ID = ', id," AND ",
+  query <- paste0('DELETE FROM ',room_table,
+                  ' WHERE ID = ', id," AND ",
                   "Date = '", date,"'")
   print(query)#for debug
   dbSendQuery(db, query)
@@ -54,34 +70,39 @@ delete <- function(id,date,day_of_the_week,room_table='room_table') {
 }
 
 
-
-
 #Automatically update the empty table of next month
-create_next_month <- function(id,room_table='room_table'){
+create_next_month <- function(id,
+                              room_table = 'room_table'){
   
-  date_to_cha<-function(date){format(date, format="%Y-%m-%d")}
+  date_to_cha <- function(date) {format(date, format = "%Y-%m-%d")}
   
-  weekday_to_cha<-function(date){format(date, format="%a")}
+  weekday_to_cha <- function(date) {format(date, format = "%a")}
   
-  room_data=loadData()
+  room_data <- loadData()
   
   for(i in 0:30){
-    if(    sum(room_data$ID==id & room_data$Date==date_to_cha(Sys.Date()+i) )==0 &  !(weekday_to_cha(Sys.Date()+i) %in% c('Sat','Sun'))     ){
+    if(sum(room_data$ID == id &
+           room_data$Date == date_to_cha(Sys.Date() + i) ) == 0 &
+       !(weekday_to_cha(Sys.Date() + i) %in% c('Sat','Sun'))){
       
-      create(id=id,date=date_to_cha(Sys.Date()+i),day_of_the_week = weekday_to_cha(Sys.Date()+i),available_period=NA,notes=NA,room_table='room_table')
-      
+      create(
+        id = id,
+        date = date_to_cha(Sys.Date() + i),
+        day_of_the_week = weekday_to_cha(Sys.Date() + i),
+        available_period = NA,
+        notes = NA,
+        room_table = 'room_table')
     }
   }
-  
 }
 
 
 
-
-
+# -------------------------------------------------------------------------
 
 #DEMO
-room<- read.csv('~/Desktop/polished fribble/individual room status.csv', stringsAsFactors = FALSE)
+
+room <- read.csv('~/Desktop/polished fribble/individual room status.csv', stringsAsFactors = FALSE)
 
 db <- dbConnect(SQLite(), 'room_store.sqlite')
 
@@ -106,13 +127,13 @@ delete(4,'2019-10-01')
 #Add the room information table in the next 30 days(counting from today)
 #The weekends aren't added
 #The dates existing in the table aren't added
-create_next_month(id=4, room_table = 'room_table')
+create_next_month(id = 4, room_table = 'room_table')
 
 
 #Note that create function cannot be used for changing existing data
 #It instead added new rows
 
-updated_room=loadData()
+updated_room <- loadData()
 print(updated_room)
 
 
@@ -128,20 +149,4 @@ print(updated_room)
 #But the package I used here(DBI) doesn't support many commands in SQL or MySQL
 #It might be useful to look at the link below for more packages doing SQL in R(if you think it's necessary)
 #http://dept.stat.lsa.umich.edu/~jerrick/courses/stat701/notes/sql.html
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
